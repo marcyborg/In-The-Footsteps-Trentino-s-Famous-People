b0cbb7ac603e99d0538dcfa59923e8e624c96812
Francesco Marchitelli <marcyborg@gmail.com>
2015-07-15 10:42:58 +0200

    Update Sparql.java

--- InTheFootsteps_Servlet/src/main/java/it/rest/utility/Sparql.java
+++ InTheFootsteps_Servlet/src/main/java/it/rest/utility/Sparql.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.rest.utility;
 
 import java.io.BufferedReader;
@@ -13,10 +8,6 @@ import java.net.URL;
 import java.net.URLEncoder;
 import org.json.JSONObject;
 
-/**
- *
- * @author paolo
- */
 public class Sparql {
     private String query ;
     private String endpoint;
@@ -30,21 +21,19 @@ public class Sparql {
     public String returnJSON(){
         String res = "";
         try{
-               URL url = new URL(endpoint);
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            	URL url = new URL(endpoint);
+	    	HttpURLConnection con = (HttpURLConnection) url.openConnection();
  
-		// optional default is GET
-               con.setDoOutput(true);
-               con.setRequestMethod("GET");
-               con.addRequestProperty("Accept", "application/json");
-            String data = URLEncoder.encode("query", "UTF-8") + "=" + URLEncoder.encode(query, "UTF-8");               
+	   	// optional default is GET
+               	con.setDoOutput(true);
+        	 con.setRequestMethod("GET");
+               	con.addRequestProperty("Accept", "application/json");
+            	String data = URLEncoder.encode("query", "UTF-8") + "=" + URLEncoder.encode(query, "UTF-8");               
                
                 OutputStreamWriter osw = new OutputStreamWriter(
                 con.getOutputStream());
                 osw.write(data);
                 osw.flush();
-
-
 
                 BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
@@ -55,35 +44,30 @@ public class Sparql {
                 while ((in = br.readLine()) != null) {
                     sb.append(in + "\n");
                 }
-
                 osw.close();
                 br.close();   
            res = sb.toString();
          return res;
         }
         catch(Exception e){e.printStackTrace();}
-        
        return res;
-        
     }
     
      public String returnHTML(){
         String res = "";
         try{
-               URL url = new URL(endpoint);
+               	URL url = new URL(endpoint);
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
  
 		// optional default is GET
-               con.setDoOutput(true);
-               con.setRequestMethod("GET");
-            String data = URLEncoder.encode("query", "UTF-8") + "=" + URLEncoder.encode(query, "UTF-8");               
+               	con.setDoOutput(true);
+               	con.setRequestMethod("GET");
+            	String data = URLEncoder.encode("query", "UTF-8") + "=" + URLEncoder.encode(query, "UTF-8");               
                
                 OutputStreamWriter osw = new OutputStreamWriter(
                 con.getOutputStream());
                 osw.write(data);
                 osw.flush();
-
-
 
                 BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
